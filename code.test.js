aafa26ce64c7a84db6c720125b9a06352842913c
Dorian Scholz <112284719+Dorian-Scholz@users.noreply.github.com>
2024-12-08 23:32:00 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const assert = require('assert');
 
-eval(fs.readFileSync('code.js')+'');
+eval(fs.readFileSync('code.js') + '');
+
+let dm;
 
 dm = [[]];
 assert(tsp_hk(dm) == 0);
@@ -9,20 +11,22 @@ assert(tsp_hk(dm) == 0);
 dm = [[0]];
 assert(tsp_hk(dm) == 0);
 
-dm = [[0,0,0],
-      [0,0,0],
-      [0,0,0]];
+dm = [[0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]];
 assert(tsp_hk(dm) == 0);
 
-dm = [[0,1,2],
-      [1,0,2],
-      [2,2,0]];
+dm = [[0, 1, 2],
+      [1, 0, 2],
+      [2, 2, 0]];
 assert(tsp_hk(dm) == 3);
 
 // https://people.sc.fsu.edu/~jburkardt/datasets/tsp/tsp.html
-dm = [[0,3,4,2,7],
-      [3,0,4,6,3],
-      [4,4,0,5,8],
-      [2,6,5,0,6],
-      [7,3,8,6,0]];
+dm = [[0, 3, 4, 2, 7],
+      [3, 0, 4, 6, 3],
+      [4, 4, 0, 5, 8],
+      [2, 6, 5, 0, 6],
+      [7, 3, 8, 6, 0]];
 assert(tsp_hk(dm) == 13);
+
+console.log("All tests passed!");
